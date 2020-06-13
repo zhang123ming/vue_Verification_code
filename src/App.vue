832a2bf46d82c51ad1704ee37a7fec27a5a88fb8
unknown <feng@qq.com>
2020-06-13 15:27:33 +0800

    code

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+width: 100%;
+height: 100%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="box">
+    <div class="code" @click="refreshCode">
+      <identify :identifyCode="identifyCode"></identify>
+    </div>
+  </div>
+</template>
+
+<script>
+import identify from "./identify";
+export default {
+  components: {
+    identify
+  },
+  data() {
+    return {
+      identifyCodes: "1234567890",
+      identifyCode: "",
+      size: 5
+    };
+  },
+  methods: {
+    // 点击刷新
+    refreshCode() {
+      this.identifyCode = "";
+      this.makeCode(this.identifyCodes, this.size);
+    },
+    makeCode(code, size) {
+      for (let i = 0; i < size; i++) {
+        this.identifyCode += code[this.randomNum(0, size)];
+      }
+      console.log("as==>", this.identifyCode);
+    },
+    // 随机数字
+    randomNum(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+  },
+  created() {
+    this.makeCode(this.identifyCodes, this.size);
+  }
+};
+</script>
+<style scoped>
+.box {
+  width: 100%;
+  height: 100%;
+  background: darkgray;
+
+}
+
+
+</style>
